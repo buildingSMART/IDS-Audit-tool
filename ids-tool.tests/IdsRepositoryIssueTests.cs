904ce73a391ee48483975677a416f3438f5477ec
Claudio Benghi <claudio.benghi@gmail.com>
2023-10-09 11:44:44 +0100

    background work for restriction base type
    See
    https://github.com/buildingSMART/IDS/issues/195#issuecomment-1747762144

---
+++ ids-tool.tests/IdsRepositoryIssueTests.cs
@@ -0,0 +1,23 @@
+﻿using idsTool.tests.Helpers;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace idsTool.tests
+{
+	public class IdsRepositoryIssueTests
+	{
+        public IdsRepositoryIssueTests(ITestOutputHelper outputHelper)
+		{
+			XunitOutputHelper = outputHelper;
+		}
+		private ITestOutputHelper XunitOutputHelper { get; }
+
+		[Fact]
+		public void Issue195_BaseRestrictionIsNotMandatory()
+		{
+			var f = new FileInfo("IssueFiles/IDS Repo/IDS Issue 195 - base restriction.ids");
+			LoggerAndAuditHelpers.FullAudit(f, XunitOutputHelper, IdsLib.Audit.Status.Ok, 0);
+		}
+	}
+}
