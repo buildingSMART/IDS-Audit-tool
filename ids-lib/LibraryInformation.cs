a73e32fd271729264dfd2011d59cbc0ae5f64c65
Claudio Benghi <claudio.benghi@gmail.com>
2025-05-19 13:09:55 +0200

    Improved dependency.
    Lowered requirement on logging abstractions to 6 on netstandard2.0
    this makes for easier management of netframework implementations.
    
    Also removed vestigial local nuget package deployment.

--- ids-lib/LibraryInformation.cs
+++ ids-lib/LibraryInformation.cs
@@ -26,5 +26,5 @@ public static class LibraryInformation
 	/// <summary>
 	/// Static field with hardcoded DLL version number. 
 	/// </summary>
-	public static string AssemblyVersion => "1.0.92";
+	public static string AssemblyVersion => "1.0.93";
 }
